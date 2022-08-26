42238dc1c4c943caf04905daece325c93c5bbb60
René Wilby <rbrki07@gmail.com>
2022-08-26 10:14:41 +0200

    update expo sdk 46

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ScrollView, StyleSheet } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { CameraPermissions } from "./components/CameraPermissions";
 import { MediaLibraryPermissions } from "./components/MediaLibraryPermissions";
@@ -11,17 +11,21 @@ import { NotificationsPermissions } from "./components/NotificationsPermissions"
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <CameraPermissions />
-        <MediaLibraryPermissions />
-        <CalendarPermissions />
-        <ContactsPermissions />
-        <LocationPermissions />
-        <NotificationsPermissions />
-      </ScrollView>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
+          <ScrollView>
+            <CameraPermissions />
+            <MediaLibraryPermissions />
+            <CalendarPermissions />
+            <ContactsPermissions />
+            <LocationPermissions />
+            <NotificationsPermissions />
+          </ScrollView>
+        </SafeAreaView>
+      </SafeAreaProvider>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </>
   );
 };
 
